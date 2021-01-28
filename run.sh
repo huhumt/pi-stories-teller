94f3c125f36b1b4c5d7b80177910233eb3e7af81
huhumt <nepood@gmail.com>
2021-01-28 19:33:20 +0800

    create config file and podcast directory if not exist

--- run.sh
+++ run.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/bash
 
-python ./src/main.py import http://www.ximalaya.com/album/257813.xml
-python ./src/main.py update
-python ./src/main.py download
+create_config_file()
+{
+    local root_directory=$(eval echo ~${SUDO_USER})
+    local config_json_filename="$root_directory/.podfox.json"
+    local path_filename="$root_directory/Music/podcast"
+
+    if [[ ! -f "$config_json_filename" ]]
+    then
+        printf "{\n    \"podcast-directory\" : \""%s"\",\n    \"maxnum\"            : 5\n}" "$path_filename" > "$config_json_filename"
+    fi
+
+    if [[ ! -d "path_filename" ]]
+    then
+        mkdir -p "$path_filename"
+    fi
+}
+
+download_podcast()
+{
+    python ./src/main.py import "$1"
+    python ./src/main.py update
+    python ./src/main.py download
+}
+
+create_config_file
+download_podcast "$1"
